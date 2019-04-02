b686459dd1a0b296d36689a692a5dc1e047c216f
elksha <43684130+elksha@users.noreply.github.com>
2019-04-02 16:11:04 +0900

    Update module.py

--- python_exercise2/python_module/module.py
+++ python_exercise2/python_module/module.py
@@ -2,17 +2,22 @@ class FourCal:
     def __init__(self, first, second):
         self.first = first
         self.second = second
+
     def add(self):
         result = self.first + self.second
         return result
+
     def mul(self):
         result = self.first * self.second
         return result
+
     def sub(self):
         result = self.first - self.second
         return result
+
     def div(self):
-        result = self.first / self.second
         if self.second == 0:
-            result = "Can't divide by zero"
+            result = "0으로는 나눌 수 없습니다."
+        if self.second != 0:
+            result = self.first / self.second
         return result
