b42d41f81ab54155e00dd1fbe42887df86482434
elksha <lksia12@naver.com>
2019-04-08 21:56:31 +0900

    190408

---
+++ crud_assignment/bookreview/book/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    ONE = '1'
+    TWO = '2'
+    THREE = '3'
+    FOUR = '4'
+    FIVE = '5'
+    Score_Choices = (
+        (ONE, '1'),
+        (TWO, '2'),
+        (THREE, '3'),
+        (FOUR, '4'),
+        (FIVE, '5'),
+    )
+    title = models.CharField(max_length=200)
+    review = models.TextField()
+    price = models.FloatField()
+    score = models.CharField(max_length=1, choices=Score_Choices)
+
+    def __str__(self):
+        return self.title
