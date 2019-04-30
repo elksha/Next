94c02c2597036d4907e2fcb97c17764f1eff24b7
elksha <lksia12@naver.com>
2019-05-01 03:34:41 +0900

    190501

--- crud_assignment/bookreview/book/forms.py
+++ crud_assignment/bookreview/book/forms.py
@@ -12,3 +12,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('comment',)
+        widgets = {
+            'comment' : forms.Textarea(attrs={'rows':2, 'cols':60}),
+        }
