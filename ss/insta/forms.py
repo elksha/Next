1ef76cd0d9aa14b8f4ea57aabe2260a3c8f5d2ca
elksha <lksia12@naver.com>
2019-05-04 18:14:25 +0900

    190504

--- ss/insta/forms.py
+++ ss/insta/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.Textarea(attrs={'rows':5, 'cols':68}), label='')
+    title = forms.CharField(widget=forms.Textarea(attrs={'rows':5, 'cols':68, 'placeholder':' 글을 입력하세요.'}), label='')
     img = forms.FileField(label='')
     class Meta():
         model = Post
         fields = ('img', 'title')
 
 class CommentForm(forms.ModelForm):
-    comment = forms.CharField(label='')
+    comment = forms.CharField(widget=forms.TextInput(attrs={'placeholder':' 댓글을 입력하세요.'}), label='')
     class Meta():
         model = Comment
         fields = ('comment', )
